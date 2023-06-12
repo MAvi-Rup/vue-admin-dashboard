1450eb7a6012c160fb7d89a0ff95596b1f3ad59b
MAvi-Rup <avirupmondal19@gmail.comm>
2023-06-12 18:34:21 +0600

    Done

--- src/components/QrCodeComponent.vue
+++ src/components/QrCodeComponent.vue
@@ -10,8 +10,8 @@
           <th>Upajilla</th>
           <th>Buying Center</th>
           <th>Tobacco Type</th>
-          <th>Action</th>
-          <th>Print</th>
+          <th>Print QrCode</th>
+          <th>Print TP</th>
           <th></th>
         </tr>
       </thead>
@@ -33,29 +33,90 @@
         </tr>
       </tbody>
     </table>
+
+    <!-- Hidden div for printable content -->
+    <div ref="printableContent" style="display: none;">
+      <h2 style="text-align: center;">Virgo Tobaco Limited</h2>
+      <h3 style="text-align: center;">Transport Permit (TP)</h3>
+      <div style="display: flex;">
+        <div style="width: 33.33%;">
+          <p v-if="transportPermitData">Farmers Name: {{ transportPermitData ? transportPermitData.farmersName : 'Loading...' }}</p>
+          <p v-if="transportPermitData">Fathers Name: {{ transportPermitData.fathersName }}</p>
+          <p v-if="transportPermitData">Village: {{ transportPermitData.village }}</p>
+          <p v-if="transportPermitData">Thana: {{ transportPermitData.thana }}</p>
+          <p v-if="transportPermitData">Zilla: {{ transportPermitData.zilla }}</p>
+        </div>
+        <div style="width: 33.33%;">
+          <img src="https://i.ibb.co/yq7hVy8/maskable-icon.png" style="display: block; margin: 0 auto;" width="100">
+        </div>
+        <div style="width: 33.33%;">
+          <p v-if="transportPermitData">Buying Date: {{ transportPermitData.buyingDate }}</p>
+          <p v-if="transportPermitData">Expiry Date: {{ transportPermitData.expiryDate }}</p>
+          <p v-if="transportPermitData">Buying Center: {{ transportPermitData.buyingCenter }}</p>
+          <p v-if="transportPermitData">Tobaco Type: {{ transportPermitData.type }}</p>
+          <p v-if="transportPermitData">Bale Quantity: {{ transportPermitData.baleQuantity }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-  
+
 <script setup>
 import { ref, onMounted } from 'vue';
-
+import printJS from 'print-js';
 import router from '../router';
 import loadTransportPermit from '../function/loadTransportPermit';
-
+import loadTransportPermitDetail from '../function/loadTransportPermitDetail';
+const farmerData = ref(null);
 const transportPermit = ref([]);
+const transportPermitData = ref({});
+const printableContent = ref(null);
 
-onMounted(loadTransportPermit(transportPermit));
+onMounted(async () => {
+  await loadTransportPermit(transportPermit);
+});
 
 const printQrCode = (id) => {
-  // Navigate to the dynamic ID-based route
-  console.log(`/all-farmers/${id}`);
+  // Implement your QR code printing logic here
+};
+
+const printTransportPermit =async (id) => {
+  try {
+    transportPermitData.value = await loadTransportPermitDetail(id);
+    console.log(transportPermitData);
+
+    if (transportPermitData.farmersName!== '' && printableContent.value) {
+      printTp();
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
-const printTransportPermit = (id) => {
-  // Navigate to the dynamic ID-based route
-  console.log(`/all-farmers/${id}`);
-}
+
+  const printTp = () => {
+  try {
+    if (printableContent.value) {
+      const iframe = document.createElement('iframe');
+      iframe.style.display = 'none';
+      document.body.appendChild(iframe);
+
+      const iframeDocument = iframe.contentDocument;
+      iframeDocument.open();
+      iframeDocument.write(printableContent.value.innerHTML);
+      iframeDocument.close();
+
+      iframe.contentWindow.onload = () => {
+        iframe.contentWindow.print();
+        document.body.removeChild(iframe);
+      };
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 </script>
-  
+
+
 <style scoped lang="scss">
 .table-container {
   overflow-x: auto;
