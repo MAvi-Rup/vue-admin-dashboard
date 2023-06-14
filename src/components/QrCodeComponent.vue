9680ebf8d200a01b769d4dd865e71b9b7b2aff3f
MAvi-Rup <avirupmondal19@gmail.comm>
2023-06-14 13:37:48 +0600

    Transport Permit Print Done

--- src/components/QrCodeComponent.vue
+++ src/components/QrCodeComponent.vue
@@ -26,52 +26,26 @@
           <td style="color: brown;">{{ farmer.type }}</td>
           <td>
             <button @click="printQrCode(farmer._id)" class="view-button">Print QrCode</button>
+            <iframe ref="printFrame" style="display: none;"></iframe>
           </td>
           <td>
-            <button @click="() => getPrintObj(farmer._id)" class="view-button">Print TP</button>
+            <button @click="() => printTransportPermit(farmer._id)" class="view-button">Print TP</button>
 
           </td>
         </tr>
       </tbody>
     </table>
-
-    <!-- Hidden div for printable content -->
-    <div ref="printableContent" style="display: none;">
-      <h2 style="text-align: center;">Virgo Tobaco Limited</h2>
-      <h3 style="text-align: center;">Transport Permit (TP)</h3>
-      <div style="display: flex;">
-        <div style="width: 33.33%;">
-          <p>Farmers Name: {{ transportPermitData ? transportPermitData.farmersName : 'Loading...' }}</p>
-          <p>Fathers Name: {{ transportPermitData ? transportPermitData.fathersName : '' }}</p>
-          <p>Village: {{ transportPermitData ? transportPermitData.village : '' }}</p>
-          <p>Thana: {{ transportPermitData ? transportPermitData.thana : '' }}</p>
-          <p>Zilla: {{ transportPermitData ? transportPermitData.zilla : '' }}</p>
-        </div>
-        <div style="width: 33.33%;">
-          <img src="https://i.ibb.co/yq7hVy8/maskable-icon.png" style="display: block; margin: 0 auto;" width="100">
-        </div>
-        <div style="width: 33.33%;">
-          <p>Buying Date: {{ transportPermitData ? transportPermitData.buyingDate : '' }}</p>
-          <p>Expiry Date: {{ transportPermitData ? transportPermitData.expiryDate : '' }}</p>
-          <p>Buying Center: {{ transportPermitData ? transportPermitData.buyingCenter : '' }}</p>
-          <p>Tobaco Type: {{ transportPermitData ? transportPermitData.type : '' }}</p>
-          <p>Bale Quantity: {{ transportPermitData ? transportPermitData.baleQuantity : '' }}</p>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import printJS from 'print-js';
-import router from '../router';
 import loadTransportPermit from '../function/loadTransportPermit';
-import loadTransportPermitDetail from '../function/loadTransportPermitDetail';
+import printTransportPermitData from '../function/printTransportPermit';
 
 const transportPermit = ref([]);
-const transportPermitData = ref({});
-const printableContent = ref(null);
+const employeeData = ref(null);
 
 onMounted(async () => {
   await loadTransportPermit(transportPermit);
@@ -82,41 +56,17 @@ const printQrCode = (id) => {
   console.log(id)
 };
 
-const getPrintObj = (id) => {
-  return {
-    id: `printableContent_${id}`,
-    popTitle: 'Transport Permit',
-    extraHead: '<meta http-equiv="Content-Language" content="en" />',
-    beforeOpenCallback(vue) {
-      transportPermitData.value = loadTransportPermitDetail(id);
-      vue.$nextTick(() => {
-        printTp(`printableContent_${id}`);
-      });
-    },
-    openCallback(vue) {
-      console.log('Executing print');
-    },
-    closeCallback() {
-      console.log('Print tool closed');
-    }
-  };
-};
-
-const printTp = (elementId) => {
-  const element = document.getElementById(elementId);
-  if (element) {
-    printJS({
-      printable: element.innerHTML,
-      type: 'html',
-      documentTitle: 'Transport Permit',
-      targetStyles: ['*']
-    });
+const printTransportPermit= async (id) => {
+  try {
+    const response = await fetch(`http://localhost:5001/transport-permit/${id}`) // Replace with your actual API endpoint
+    employeeData.value = await response.json();
+    printTransportPermitData(employeeData);
+  } catch (error) {
+    console.error('Error fetching employee data:', error);
   }
-};
+}; 
+
 </script>
-
-
-
 
 <style scoped lang="scss">
 .table-container {
--- src/views/LoanSanctionView.vue
+++ src/views/LoanSanctionView.vue
@@ -1,97 +1,28 @@
 <template>
   <div>
-    <button @click="printEmployeeData">Print Employee Data</button>
+    <button @click="printTransportPermit(farmers._id)">Print Employee Data</button>
+    
   </div>
 </template>
-
 
 <script setup>
 import { ref } from 'vue';
 
-const employeeData = ref(null);
 
-const printEmployeeData = async () => {
-  try {
-    const response = await fetch(`http://localhost:5001/transport-permit/6485acad6f910cdf1034ca68`); // Replace with your actual API endpoint
-    employeeData.value = await response.json();
-    printData();
-  } catch (error) {
-    console.error('Error fetching employee data:', error);
-  }
-};
 
-const printData = () => {
-  const printDocument = document.createElement('div');
-  printDocument.innerHTML = `
-    <html>
-      <head>
-        <style>
-          @media print {
-            #print-template {
-              display: grid;
-              grid-template-columns: 33.33% 33.33% 33.33%;
-              justify-content: center;
-            }
-            .header {
-              text-align: center;
-            }
-            .content {
-              display: grid;
-              grid-template-columns: 1fr 1fr 1fr;
-              gap: 10px;
-            }
-            .middle-column {
-              display: flex;
-              align-items: center;
-              justify-content: center;
-            }
-          }
-        </style>
-      </head>
-      <body>
-        <div id="print-template">
-          <div class="header">
-            <h1>Virgo Tobaco Limited</h1>
-            <h2>Transport Permit (TP)</h2>
-          </div>
-          <div class="content">
-            <div class="left-column">
-              <h3>Farmer's ID: ${employeeData.value.id}</h3>
-              <h3>Farmer's Name: ${employeeData.value.farmersName}</h3>
-              <h3>Mobile Number: ${employeeData.value.mobileNo}</h3>
-              <h3>Zilla: ${employeeData.value.farmerJilla}</h3>
-              <h3>UpZilla: ${employeeData.value.farmerUpjilla}</h3>
-            </div>
-            <div class="middle-column">
-              <img src="${employeeData.value.imageURL}" alt="Employee Image">
-            </div>
-            <div class="right-column">
-              <h3>Buying Center: ${employeeData.value.buyingCenter}</h3>
-              <h3>Buying Date: ${employeeData.value.buyingDate}</h3>
-              <h3>Expiry Date: ${employeeData.value.expiryDate}</h3>
-              <h3>Tobacco Type: ${employeeData.value.type}</h3>
-              <h3>Bale Quantity: ${employeeData.value.baleQuantity}</h3>
-            </div>
-          </div>
-        </div>
-      </body>
-    </html>
-  `;
 
-  const printWindow = window;
-  printWindow.document.write(printDocument.innerHTML);
-  printWindow.document.close();
-  printWindow.print();
-};
+
+
 </script>
 
 
 
 
 
-<style lang="scss" scoped>
-h1{
-    text-align: center;
-}
 
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+}
 </style>
